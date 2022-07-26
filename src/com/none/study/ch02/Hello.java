c621364f7bc755550ed9b5c644bd61b3fe848f71
seungjun12 <vndan110@naver.com>
2022-07-26 15:58:26 +0900

    20220726 1558

---
+++ src/com/none/study/ch02/Hello.java
@@ -0,0 +1,32 @@
+package com.none.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//첫 시작!
+		
+		boolean abc = true;
+		System.out.println(abc);
+		
+		String name="박승준";
+		
+		String seat_red1 ="1루 레드석";
+		String seat_red2 ="3루 레드석";
+		String seat_pupple1="1루 보라석";
+		String seat_pupple3="3루 보라석";
+		String seat_normal="일반석";
+		String seat_hemerunCouple="홈런 커플존";
+		String seat_table_sky="스카이 탁자석";
+		String seat_table="테이블석";
+		String seat_family="외야패밀리";
+		String seat_bbq="bbq존";
+		String seat_green="그린존";
+		String seat_vip="vip존";
+		
+		
+		
+		
+		
+		
+	}//main() end
+}//class end
