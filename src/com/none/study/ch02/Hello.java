e5d573fed9a07d6a7d153a8335a4e91b01884400
seungjun12 <vndan110@naver.com>
2022-07-26 16:10:49 +0900

    20220726 1610

--- src/com/none/study/ch02/Hello.java
+++ src/com/none/study/ch02/Hello.java
@@ -11,17 +11,46 @@ public class Hello {
 		String name="박승준";
 		
 		String seat_red1 ="1루 레드석";
-		String seat_red2 ="3루 레드석";
+		String seat_red3 ="3루 레드석";
 		String seat_pupple1="1루 보라석";
 		String seat_pupple3="3루 보라석";
 		String seat_normal="일반석";
-		String seat_hemerunCouple="홈런 커플존";
+		String seat_homerunCouple="홈런 커플존";
 		String seat_table_sky="스카이 탁자석";
 		String seat_table="테이블석";
 		String seat_family="외야패밀리";
 		String seat_bbq="bbq존";
 		String seat_green="그린존";
 		String seat_vip="vip존";
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은:" +name);
+////////////////////////////////////////////////////////////////////////////////
+		System.out.println("---------------------------------------");
+		System.out.println("seat_red1은?" + seat_red1);
+		System.out.println(seat_red3);
+		System.out.println(seat_pupple1);
+		System.out.println(seat_pupple3);
+		System.out.println("내가 맨날 앉는곳..." +  seat_normal);
+		System.out.println("한번도 안가본곳" +  seat_homerunCouple);
+		System.out.println(seat_table_sky);
+		System.out.println("11111"+seat_family);
+		System.out.println(seat_table);
+		System.out.println(seat_bbq);
+		System.out.println(seat_green);
+		System.out.println(seat_vip);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
