78d11e96b2079d2180c927dced93ed8238477297
seungjun12 <vndan110@naver.com>
2022-07-29 15:51:34 +0900

    20220729 1551

---
+++ src/com/none/study/ch02/InExample.java
@@ -0,0 +1,17 @@
+package com.none.study.ch02;
+
+public class InExample {
+
+	public static void main(String[] args) {
+		
+		int var1 = 10; 	//십진수
+		int var2 = 012; //8진수
+		int var3 = 0xa; //16진수
+
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+	}//main() end
+}//class end
