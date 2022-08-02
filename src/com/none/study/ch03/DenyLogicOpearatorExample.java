fdc3e8103b29b953a1cb1d5c64868d08827ec193
seungjun12 <vndan110@naver.com>
2022-08-02 17:29:33 +0900

    20220802 1729

--- src/com/none/study/ch03/DenyLogicOpearatorExample.java
+++ src/com/none/study/ch03/DenyLogicOpearatorExample.java
@@ -4,6 +4,21 @@ public class DenyLogicOpearatorExample {
 
 	public static void main(String[] args) {
 
-
+		boolean play = true;
+		System.out.println(play);
+		
+		play=!play;
+		System.out.println(play);
+		
+		play=!play;
+		System.out.println(play);
+		
+		int j=3;
+		if(!(j==3)) {
+			System.out.println("3이 아님");
+		}else {
+			System.out.println("3임");
+		}//if end
+		
 	}//main() end
 }//class end
