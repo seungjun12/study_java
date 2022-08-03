3e25bbc6662c30a6615783e2fc52954e2ff33633
seungjun12 <vndan110@naver.com>
2022-08-03 17:13:58 +0900

    20220803 1713

--- src/com/none/study/ch03/ArithmeticOperatorExample.java
+++ src/com/none/study/ch03/ArithmeticOperatorExample.java
@@ -27,8 +27,44 @@ public class ArithmeticOperatorExample {
 	
 		double result7=(double)v1/v2;
 		System.out.println("result7:" + result7);
+		
+		////////////////////////////////////////////////
+		
+		double a = 5./3;
+		double b = 5./8;
+		double c = a-b;
+		double d = 49./5;
+		double dap= c*d;
+		System.out.println("1번 정답은" +dap);
+		
+		double e =11./6.;
+		double f = 3.25-e;
+		double g = 12./17;
+		double h = f*g;
+		/* System.out.println(h); */
+		double dap2= 5.-h;
+		System.out.println("2번 정답은" +dap2);
+		
+		sum(8,7,5);
+		sum(1,9,5);
+		sum(5,6,6);
+		sum(4,7,1);
+		sum(7,5,5);
 	
-	
-	
+		average(60,70,85,90,45);
+		average(50,75,70,60,70);
+		average(55,60,64,58,90);
+		average(80,70,63,88,78);
 	}//main() end
+	
+	public static void sum(int x, int y, int z) {
+		System.out.println(x + "+" + y + "+" + z + "=" + (x+y+z));
+	}//sum() end
+	
+	public static void average(int eng ,int mat ,int phy,int mus, int ath) {
+		double result = (double)(eng+mat+phy+mus+ath)/5;
+		System.out.println("평균:" +result);
+	}//hap end
+
+
 }//class end
---
+++ src/com/none/study/ch03/StringConcatExample.java
@@ -0,0 +1,13 @@
+package com.none.study.ch03;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		
+		String stringValue = "jdk";
+		String stringValue2 = "1.11";
+		System.out.println("우리가 사용하는 자바는 " + stringValue+" " +stringValue2+"입니다.");
+
+
+	}//main() end
+}//class end
