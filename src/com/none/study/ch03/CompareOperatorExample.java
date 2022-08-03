0cba868370564837cc99d0dd3e7e5a3f2df13bbc
seungjun12 <vndan110@naver.com>
2022-08-03 17:25:38 +0900

    20220803 1725

---
+++ src/com/none/study/ch03/CompareOperatorExample.java
@@ -0,0 +1,28 @@
+package com.none.study.ch03;
+
+public class CompareOperatorExample {
+
+	public static void main(String[] args) {
+
+		int num1 = 30;
+		int num2 = 30;
+		
+		boolean result = (num1==num2);
+		System.out.println(result);
+		
+		boolean result2 = (num1!=num2);
+		System.out.println(result2);
+		
+		boolean result3 = (num1<num2);
+		System.out.println(result3);
+		
+		boolean result4 = (num1<=num2);
+		System.out.println(result4);
+		
+		boolean result5 = (num1>num2);
+		System.out.println(result5);
+		
+		boolean result6 = (num1>=num2);
+		System.out.println(result6);
+	}//main() end
+}//class end
