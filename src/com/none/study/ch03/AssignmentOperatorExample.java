1dfd17ee0e8096403d467f485bd22be4b43284fa
seungjun12 <vndan110@naver.com>
2022-08-05 11:23:12 +0900

    20220805 1123

---
+++ src/com/none/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,20 @@
+package com.none.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		
+		int result = 2;
+		result += 10;
+		System.out.println("result: " + result);
+
+		int result2 =2;
+		result2 -= 20;
+		System.out.println("result2: " + result2);
+		
+		int result3 =2;
+		result3 *=3;
+		System.out.println("result3: " + result3);
+		
+	}//main() end
+}//class end
