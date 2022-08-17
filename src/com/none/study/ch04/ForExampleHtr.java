84fb8f28b452ff1c4b02065b6bbc502cae9a6d6c
seungjun12 <vndan110@naver.com>
2022-08-17 17:30:43 +0900

    20220817 1730

---
+++ src/com/none/study/ch04/ForExampleHtr.java
@@ -0,0 +1,48 @@
+package com.none.study.ch04;
+
+public class ForExampleHtr {
+
+	public static void main(String[] args) {
+		
+		//1
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=10;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+				
+		System.out.println("-----------------------------");
+		
+		//2
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=10;j++) {
+				if(i>=2 && i<=4) {
+					if(j>=2 && j<=9) {
+						System.out.print(" ");
+					}else {
+						System.out.print("*");
+					}//if end
+				}else {
+					System.out.print("*");
+				}//if end
+			}//for end
+			System.out.println();
+		}//for end
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}//main() end
+}//class end
