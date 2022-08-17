52799f6bb93b5ff395eb09ee97fd5cd5cc02c591
seungjun12 <vndan110@naver.com>
2022-08-17 15:44:30 +0900

    20220817 1544

--- src/com/none/study/ch04/ForExample.java
+++ src/com/none/study/ch04/ForExample.java
@@ -9,10 +9,21 @@ public class ForExample {
 			System.out.println("***********");
 		}//for end
 		
+		System.out.println("----------------------------");
+		
+		//1 선생님
+		
+		for(int i=1 ; i<=5 ; i++) {
+			for(int j=1 ;j<=10;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 		System.out.println("-------------");
 		
 		//2
-		for(int i=1;i<=5;i++) {
+		for(int i=1 ;i<=5 ;i++) {
 			if(i>=2 && i<=4) {
 				System.out.println("*        *");
 			}else {
@@ -21,6 +32,23 @@ public class ForExample {
 		}//for end
 		
 		System.out.println("-------------");
+		
+		//2 선생님
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=10;j++) {
+				if(i>=2 && i<=4) {
+					if(j>=2 && j<=9) {
+						System.out.print(" ");
+					}else {
+						System.out.print("*");
+					}//if end
+				}else {
+					System.out.print("*");
+				}//if end
+			}
+			System.out.println();
+		}
 		
 		//3
 /*망한 버전
@@ -105,6 +133,28 @@ public class ForExample {
 				System.out.println(a);
 			}//if end
 		}//for end
+		
+		System.out.println("--------------------------------");
+		
+		//3 선생님 버전
+		
+		for(int i=1 ;i<=10 ; i++) {
+			if(i==10) {
+				for(int j=1;j<=i;j++) {
+					if(j%2==1) {
+						System.out.print(j);
+					}else {
+						System.out.print(" ");
+					}//if end
+				}//for end
+				System.out.println("0");
+			}else {
+				for(int j=1;j<=i;j++) {
+					System.out.print(j);
+				}//for end
+				System.out.println();
+			}//if end
+		}
 				
 		
 		
