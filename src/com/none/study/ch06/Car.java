83cc056e33a79888b88e59f139be5b629feadcf8
seungjun12 <vndan110@naver.com>
2022-08-22 15:59:37 +0900

    20220822 1559

---
+++ src/com/none/study/ch06/Car.java
@@ -0,0 +1,14 @@
+package com.none.study.ch06;
+
+public class Car {
+
+	
+	//field 변수
+	int a=1;
+	String name="장원영";
+	String [] name2= {"장원영","고소영"};
+	
+	
+	//method 함수
+	
+}//class end
---
+++ src/com/none/study/ch06/CarExample.java
@@ -0,0 +1,14 @@
+package com.none.study.ch06;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+
+		Car car = new Car();
+		
+		System.out.println(car.a);
+		System.out.println(car.name);
+		System.out.println(car.name2[1]);
+		
+	}//main() end
+}//class end
