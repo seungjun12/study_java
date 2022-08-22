33f2ea99f54d278c229d34012f4390e3bb11a3ee
seungjun12 <vndan110@naver.com>
2022-08-22 16:27:44 +0900

    20220822 1627

---
+++ src/com/none/study/ch06/Iphone.java
@@ -0,0 +1,17 @@
+package com.none.study.ch06;
+
+public class Iphone {
+
+	
+	//매개변수 field
+	String name ="Iphone";
+	String [] name2 = {"아이폰7","아이폰8","아이폰x","아이폰xs","아이폰11","아이폰12","아이폰13"};
+	
+	
+	//method 함수
+	
+	public void sj() {
+		System.out.println("나는 짱이다");
+	}
+	
+}//class end
---
+++ src/com/none/study/ch06/IphoneExample.java
@@ -0,0 +1,22 @@
+package com.none.study.ch06;
+
+public class IphoneExample {
+
+	public static void main(String[] args) {
+
+		Iphone iphone = new Iphone();
+		
+		System.out.println(iphone.name);
+		System.out.println(iphone.name2[3]);
+		
+		iphone.sj();
+		
+		
+		
+		Car car = new Car();
+		
+		System.out.println(car.name);
+		System.out.println(car.name2[1]);
+		
+	}//main() end
+}//class end
