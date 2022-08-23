6701bb42854db196dd594a57e2c43a1104926151
seungjun12 <vndan110@naver.com>
2022-08-23 15:08:54 +0900

    20220823 1508

--- src/com/none/study/ch06/Arithmetic.java
+++ src/com/none/study/ch06/Arithmetic.java
@@ -1,5 +1,62 @@
 package com.none.study.ch06;
 
 public class Arithmetic {
-
+	
+	int a=12;
+	int b=3;
+	private String hap;
+	
+	//덧셈
+	public int sumReturn (int a, int b) {
+		int sum=0;
+		sum=a+b;
+		return sum;		
+	}//sumReturn() end
+	
+	//뺄셈
+	public int minusReturn(int a,int b) {
+		int minus=0;
+		minus=a-b;
+		return minus;
+	}//minusReturn() end
+	
+	//곱셈
+	public int multiplication(int a,int b) {
+		int multiplication=0;
+		multiplication=a*b;
+		return multiplication;
+	}//multiplication() end
+	
+	//나눗셈
+	public int division(int a,int b) {
+		int division=0;
+		division=a/b;
+		return division;
+	}//division() end
+	
+	//나머지
+	public int remainder(int a,int b) {
+		int remainder=0;
+		remainder=a%b;
+		return remainder;
+	}//remainder() end
+	
+	public void say(int a) {
+		System.out.println("값은:"+a);
+	}
+	
+	
+	
+	///////////////////////////////////////////////
+	
+	public String name(String name1,String name2) {
+		String name="";
+		name=name1+name2;
+		return name;
+	}
+	
+	public void hap(String name3) {
+		System.out.println(name3);
+	}
+	
 }//class end
--- src/com/none/study/ch06/ArithmeticExample.java
+++ src/com/none/study/ch06/ArithmeticExample.java
@@ -3,8 +3,28 @@ package com.none.study.ch06;
 public class ArithmeticExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Arithmetic arithmetic = new Arithmetic();
+		
+		int resultAdd =arithmetic.sumReturn(3, 5);
+		int resultMinus =arithmetic.minusReturn(3, 5);
+		int resultMultiplication =arithmetic.multiplication(3, 5);
+		int resultDivison =arithmetic.division(3, 5);
+		int resultRemainder =arithmetic.remainder(3, 5);
+		
+		
+		arithmetic.say(resultAdd);
+		arithmetic.say(resultMinus);
+		arithmetic.say(resultMultiplication);
+		arithmetic.say(resultDivison);
+		arithmetic.say(resultRemainder);
+		
+		/////////////////////////////////////
+		
+		String resultName =arithmetic.name("박박", "승준");
+		
+		arithmetic.hap(resultName);
+		
 
-	}
-
-}
+	}//main() end
+}//class end
