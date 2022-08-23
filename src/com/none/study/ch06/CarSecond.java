c613be533577baea535957c70b57dd76da5a0e8e
seungjun12 <vndan110@naver.com>
2022-08-23 17:29:41 +0900

    20220823

--- src/com/none/study/ch06/CarSecond.java
+++ src/com/none/study/ch06/CarSecond.java
@@ -2,30 +2,82 @@ package com.none.study.ch06;
 
 public class CarSecond {
 
-	String color="red";
-	int cc = 1500;
-	String name ="아반때";
+//	String color="red";
+//	int cc = 1500;
+//	String name ="아반때";
+//	
+//	
+//	CarSecond(String name){
+//		this.name=name;
+//	}
+//	
+//	CarSecond(String name,int cc){
+//		this.name=name;
+//		this.cc =cc;
+//	}
+//	
+//	CarSecond(String name,int cc,String color){
+//		this.name=name;
+//		this.cc=cc;
+//		this.color=color;
+//	}
+//	
+//	//기본생성자
+//	CarSecond(){
+//		
+//	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	String name;
+	String color;
+	int cn;
+	
+	//기본 생성자 함수
+	CarSecond() {
+
+	}
 	
 	CarSecond(String name){
 		this.name=name;
 	}
 	
-	CarSecond(String name,int cc){
-		this.name=name;
-		this.cc =cc;
-	}
-	
-	CarSecond(String name,int cc,String color){
-		this.name=name;
-		this.cc=cc;
+	CarSecond(String color,int cn){
 		this.color=color;
+		this.cn=cn;
 	}
 	
-	//기본생성자
-	CarSecond(){
-		
+	CarSecond(String name,String color,int cn){
+		this.name=name;
+		this.color=color;
+		this.cn=cn;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }//class end
--- src/com/none/study/ch06/CarSecondExample.java
+++ src/com/none/study/ch06/CarSecondExample.java
@@ -4,39 +4,74 @@ public class CarSecondExample {
 
 	public static void main(String[] args) {
 
-		CarSecond carsecond = new CarSecond();
-		String color;
-		
-		CarSecond carsecond2 = new CarSecond("blue");
-		CarSecond carsecond3 = new CarSecond("white",3000);
-		String color2="Red";
-		
-		System.out.println("carSecond2.color:"+carsecond2.color);
-		System.out.println("CarSecond2.cc:"+carsecond2.cc);
-		
-		System.out.println("carsecond3.color:"+carsecond3.color);
-		System.out.println("carsecond3.cc:"+carsecond3.cc);
-	
-		CarSecond carsecond4 = new CarSecond("소나타");
-		CarSecond carsecond5 = new CarSecond("그랜저",2000);
-		CarSecond carsecond6 = new CarSecond("베뉴",5000,"black");
-		
-		System.out.println("-----------------------------");
-		
-		
-		
-		System.out.println("carsecond4.name:"+carsecond4.name);
-		System.out.println("carsecond4.cc:"+carsecond4.cc);
-		System.out.println("carsecond4.color:"+carsecond4.color);
-	
-		System.out.println("carsecond5.name:"+carsecond5.name);
-		System.out.println("carsecond5.cc:"+carsecond5.cc);
-		System.out.println("carsecond5.color:"+carsecond5.color);
+//		CarSecond carsecond = new CarSecond();
+//		String color;
+//		
+//		CarSecond carsecond2 = new CarSecond("blue");
+//		CarSecond carsecond3 = new CarSecond("white",3000);
+//		String color2="Red";
+//		
+//		System.out.println("carSecond2.color:"+carsecond2.color);
+//		System.out.println("CarSecond2.cc:"+carsecond2.cc);
+//		
+//		System.out.println("carsecond3.color:"+carsecond3.color);
+//		System.out.println("carsecond3.cc:"+carsecond3.cc);
+//	
+//		CarSecond carsecond4 = new CarSecond("소나타");
+//		CarSecond carsecond5 = new CarSecond("그랜저",2000);
+//		CarSecond carsecond6 = new CarSecond("베뉴",5000,"black");
+//		
+//		System.out.println("-----------------------------");
+//		
+//		
+//		
+//		System.out.println("carsecond4.name:"+carsecond4.name);
+//		System.out.println("carsecond4.cc:"+carsecond4.cc);
+//		System.out.println("carsecond4.color:"+carsecond4.color);
+//	
+//		System.out.println("carsecond5.name:"+carsecond5.name);
+//		System.out.println("carsecond5.cc:"+carsecond5.cc);
+//		System.out.println("carsecond5.color:"+carsecond5.color);
+//
+//		System.out.println("carsecond6.name:"+carsecond6.name);
+//		System.out.println("carsecond6.cc:"+carsecond6.cc);
+//		System.out.println("carsecond6.color:"+carsecond6.color);
 
-		System.out.println("carsecond6.name:"+carsecond6.name);
-		System.out.println("carsecond6.cc:"+carsecond6.cc);
-		System.out.println("carsecond6.color:"+carsecond6.color);
+		
+		CarSecond carsecond = new CarSecond("아반떼");
+		CarSecond carsecond2 = new CarSecond("검정",4405);
+		CarSecond carsecond3 = new CarSecond("아반떼","흰색",1234);
+		
+		System.out.println(carsecond.name);
+		System.out.println(carsecond2.color);
+		System.out.println(carsecond2.cn);
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
 	}//main() end
 }//class end
