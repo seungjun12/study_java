46ae3d03900abaea4443d6e2e6d9a3902da88a87
seungjun12 <vndan110@naver.com>
2022-08-23 17:19:52 +0900

    20220823 1719

---
+++ src/com/none/study/ch06/Korean.java
@@ -0,0 +1,25 @@
+package com.none.study.ch06;
+
+public class Korean {
+
+	String name="";
+	String ssn="";
+	int age=1;
+	
+	Korean(String name,String ssn){
+		this.name=name;
+		this.ssn=ssn;
+	}
+	
+	Korean(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+	
+	
+	//기본생성자 함수
+	public Korean() {
+		
+	}
+	
+}//class end
