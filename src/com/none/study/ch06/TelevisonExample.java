24769a26929f653a63015a424f3172a621433f46
seungjun12 <vndan110@naver.com>
2022-08-24 17:18:42 +0900

    20220824 1718

--- src/com/none/study/ch06/TelevisonExample.java
+++ src/com/none/study/ch06/TelevisonExample.java
@@ -7,8 +7,18 @@ public class TelevisonExample {
 		System.out.println(Televison.company);
 		System.out.println(Televison.model);
 		System.out.println(Televison.info);
+
+		
 		
 //		System.out.println(Televison.version);
+//      오류인 이유를 설명해보시오
+//		static으로 안해서
+//		
+//		그럼 사용할려면 객체를 생성해 주어야 한다
+//      ㄱㄱ
+		
+		Televison televison = new Televison();
+		System.out.println(televison.version);
 		
 		
 
