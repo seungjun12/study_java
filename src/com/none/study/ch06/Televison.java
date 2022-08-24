70ea045257bc1877592e880efbd5d53ff3c32775
seungjun12 <vndan110@naver.com>
2022-08-24 16:43:04 +0900

    20220824 1642

---
+++ src/com/none/study/ch06/Televison.java
@@ -0,0 +1,16 @@
+package com.none.study.ch06;
+
+public class Televison {
+
+	public static String company ="samsung";
+	public static String model="oled";
+	public static String info;
+	
+	String version ="1.0";
+	
+	
+	//이것이 바로 static 블록!
+	static {
+		info= company+" " +model;
+	}
+}//class end
---
+++ src/com/none/study/ch06/TelevisonExample.java
@@ -0,0 +1,16 @@
+package com.none.study.ch06;
+
+public class TelevisonExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Televison.company);
+		System.out.println(Televison.model);
+		System.out.println(Televison.info);
+		
+//		System.out.println(Televison.version);
+		
+		
+
+	}//main() end
+}//class end
