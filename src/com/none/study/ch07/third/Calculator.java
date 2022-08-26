6f255141aeb0bcfdfc350d00770deb4382fdc729
seungjun12 <vndan110@naver.com>
2022-08-26 16:47:01 +0900

    20220826 1646

---
+++ src/com/none/study/ch07/third/Calculator.java
@@ -0,0 +1,11 @@
+package com.none.study.ch07.third;
+
+public class Calculator {
+
+	public double areaCircle(double r) {
+		System.out.println("Calculator 객체의 areaCircle 실행");
+		return 3.14159*r*r;
+	}	
+	
+	
+}//class end
---
+++ src/com/none/study/ch07/third/Computer.java
@@ -0,0 +1,20 @@
+package com.none.study.ch07.third;
+
+public class Computer extends Calculator {
+
+//	@Override
+//	public double areaCircle(double r) {
+//		// TODO Auto-generated method stub
+//		return super.areaCircle(r);
+//	}
+
+	@Override
+	public double areaCircle(double r) {
+		System.out.println("Computer 객체의 areaCircle 실행");
+		return Math.PI*r*r;
+	}
+	
+	
+
+	
+}
